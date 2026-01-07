783e6c8d0d865e600f47d27bf3c1350844602436
Vasquez Lazo <jdvasquez@indracompany.com>
2026-01-07 15:58:03 -0500

    3.4 1:18, token listo

--- src/app/core/guards/session.guard.ts
+++ src/app/core/guards/session.guard.ts
@@ -1,6 +1,28 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { inject } from '@angular/core';
+
+
+const checkSession = (): boolean => {
+  try{
+    const cookieService = inject(CookieService);
+    const token = cookieService.check('token');
+    return token;
+  } catch(error){
+    return false;
+  }
+};
+
 
 export const sessionGuard: CanActivateFn = (route, state) => {
+  const isValidSession = checkSession();
+  const router = inject(Router);
+  if (!isValidSession) {
+    console.warn('No valid session, redirecting to /auth/login');
+    router.navigate(['/', 'auth', 'login']);
+    return false;
+  }
+
   return true;
 };
 
--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environments';
 import { Observable, tap } from 'rxjs';
+//import { CookieService } from 'ngx-cookie-service/lib/cookie.service';
+import { CookieService } from 'ngx-cookie-service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +13,8 @@ import { Observable, tap } from 'rxjs';
 export class AuthService {
 
   private readonly URL = environment.api;
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+     private cookieService: CookieService) { }
 
   sendCredentials(email: string, password: string) : Observable<any> {
 
@@ -22,8 +26,15 @@ export class AuthService {
     console.log('Sending credentials', body);
 
     return this.httpClient.post<any>(`${this.URL}/auth/login`, body).pipe(
-        tap(({data}) => {
-           // console.log('User logged in:', data);
+        tap((data) => {
+           console.log('Response data:', data);
+           const token = data.tokenSession || data.token || data.accessToken;
+           if (token) {
+             this.cookieService.set('token', token, 4, '/');
+             console.log('Token saved:', token);
+           } else {
+             console.error('Token not found in response:', data);
+           }
         })
     );      
   }
